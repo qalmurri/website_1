cf21085d1bfeb5eebe38d834b159fb48e86d6cc5
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-18 15:11:37 +0700

    Add files via upload

--- src/function/check_setting.php
+++ src/function/check_setting.php
@@ -2,27 +2,31 @@
 session_start();
 require('../../src/config/database.php');
 
-if (!empty($_SESSION['id_section'])) {
-    $id = $_GET['id'];
+if ($_SESSION['token'] == "1") {
+    if (!empty($_SESSION['id_section'])) {
+        $id = $_GET['id'];
 
-    $process = "SELECT *
+        $process = "SELECT *
 FROM login
 where id_member=$id";
-    $query = $dbuser->prepare($process);
-    $query->execute();
-    $setting = $query->fetch(PDO::FETCH_ASSOC);
-    $_SESSION['id_setting'] = $setting['id_setting'];
+        $query = $dbuser->prepare($process);
+        $query->execute();
+        $setting = $query->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['id_setting'] = $setting['id_setting'];
 
-    $id_section = $setting['id_section'];
+        $id_section = $setting['id_section'];
 
-    $process2 = "SELECT name_section
+        $process2 = "SELECT name_section
 FROM section
 where id_section=$id_section";
-    $query2 = $dbuser->prepare($process2);
-    $query2->execute();
-    $section = $query2->fetch(PDO::FETCH_ASSOC);
+        $query2 = $dbuser->prepare($process2);
+        $query2->execute();
+        $section = $query2->fetch(PDO::FETCH_ASSOC);
 
-    echo '<script>alert("Login Sukses");window.location="../../' . $section['name_section'] . '/index.php"</script>';
+        echo '<script>alert("Login Sukses");window.location="../../' . $section['name_section'] . '/index.php"</script>';
+    } else {
+        echo '<script>alert("Gak ada ID Section");window.location="../../index.php"</script>';
+    }
 } else {
-    echo '<script>alert("Gak ada ID Section");window.location="../../index.php"</script>';
+    echo '<script>alert("Gak ada Token");window.location="../../logout.php";</script>';
 }
---
+++ web/page/books/index/spec1.php
@@ -0,0 +1,69 @@
+<table class='change-spec info-content'>
+    <tr>
+        <td>
+            <p>Publisher</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>Universitas Brawijaya</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <p>Category</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>Pendidikan</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <p>Pages</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>485</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <p>ISBN</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>978-602-397-482-5</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <p>Size</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>16cm X 25cm</p>
+            <p>HVS 70 gsm</p>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <p>Harga</p>
+        </td>
+        <td>
+            <p>:</p>
+        </td>
+        <td>
+            <p>Rp. 150.000,00</p>
+        </td>
+    </tr>
+</table>
