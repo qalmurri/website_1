85b666b07c19d1a14445b30b1019cc39e4cd1acf
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-18 08:08:14 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   style.css
    	modified:   web/page/books/index.php

--- web/page/books/index.php
+++ web/page/books/index.php
@@ -7,42 +7,236 @@
             </div>
             <h1 title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis"><a class="super">[100] </a><a href="www.google.com">Teknik Praktis Pengecatan Imunohistokimia Imunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisUntuk Penelitian dan Aplikasi Klinis</a></h1>
             <hr>
-            <div class="info-content">
-                <div class="thumbnail">
-                    <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
-                </div>
-
-                <div class="specification">
-                    <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+            <table class="info-content">
+                <tr>
+                    <td class="thumbnail">
+                        <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
+                    </td>
+                    <td class="specification">
+                        <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+                            <hr>
+                        <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
+                        <p class="pages"><b>Pages: </b>500</p>
+                        <div id="select"></div>
                         <hr>
-                    <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
-                    <p class="pages"><b>Pages: </b>500</p>
-                    <div id="select" class="info-content"></div>
 
-                    <select id="id_buku" onchange="myFunction()">
-                        <option value="
+                        <select id="id_buku" onchange="myFunction()">
+                            <option value="
                 <p class='isbn'><b>ISBN: </b>978-602-397-482-5</p>
                 <p class='size'><b>Size: </b>16cm X 25cm</p>
                 <p class='gsm'>548 Pages - HVS 70 gsm</p>
                 <p class='price'><b>Harga: </b>Rp. 150.000,00</p>
                 ">ISBN</option>
-                        <option value="
+                            <option value="
                 <p class='isbn'><b>ISBN: </b>978-602-397-481-8</p>
                 <p class='size'><b>Size: </b>16cm X 25cm</p>
                 <p class='gsm'>548 Pages - PDF</p>
                 <p class='price'><b>Harga: </b>Rp. 75.000,00</p>
                 ">E-ISBN</option>
-                    </select>
-                    <script>
-                        function myFunction() {
-                            var x = document.getElementById("id_buku").value;
-                            document.getElementById("select").innerHTML = x;
-                        }
-                        myFunction();
-                    </script>
-                </div>
-            </div>
+                        </select>
+                        <script>
+                            function myFunction() {
+                                var x = document.getElementById("id_buku").value;
+                                document.getElementById("select").innerHTML = x;
+                            }
+                            myFunction();
+                        </script>
+
+                    </td>
+                </tr>
+            </table>
         </div>
         <!-- FIX -->
+
+        <div class="grid-content">
+            <div class="info">
+                <p class="date">Januari 2022</p>
+                <p class="author" title="Bambang Prawiro"><b>Author: </b><a href="google.com">Bambang Prawiro</a></p>
+            </div>
+            <h1 title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis"><a class="super">[100] </a><a href="www.google.com">Teknik Praktis Pengecatan Imunohistokimia Imunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisUntuk Penelitian dan Aplikasi Klinis</a></h1>
+            <hr>
+            <table class="info-content">
+                <tr>
+                    <td class="thumbnail">
+                        <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
+                    </td>
+                    <td class="specification">
+                        <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+                            <hr>
+                        <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
+                        <p class="pages"><b>Pages: </b>500</p>
+                        <div id="select"></div>
+                        <hr>
+
+                        <select id="id_buku" onchange="myFunction()">
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-482-5</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - HVS 70 gsm</p>
+                <p class='price'><b>Harga: </b>Rp. 150.000,00</p>
+                ">ISBN</option>
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-481-8</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - PDF</p>
+                <p class='price'><b>Harga: </b>Rp. 75.000,00</p>
+                ">E-ISBN</option>
+                        </select>
+                        <script>
+                            function myFunction() {
+                                var x = document.getElementById("id_buku").value;
+                                document.getElementById("select").innerHTML = x;
+                            }
+                            myFunction();
+                        </script>
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+        <div class="grid-content">
+            <div class="info">
+                <p class="date">Januari 2022</p>
+                <p class="author" title="Bambang Prawiro"><b>Author: </b><a href="google.com">Bambang Prawiro</a></p>
+            </div>
+            <h1 title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis"><a class="super">[100] </a><a href="www.google.com">Teknik Praktis Pengecatan Imunohistokimia Imunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisUntuk Penelitian dan Aplikasi Klinis</a></h1>
+            <hr>
+            <table class="info-content">
+                <tr>
+                    <td class="thumbnail">
+                        <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
+                    </td>
+                    <td class="specification">
+                        <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+                            <hr>
+                        <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
+                        <p class="pages"><b>Pages: </b>500</p>
+                        <div id="select"></div>
+                        <hr>
+
+                        <select id="id_buku" onchange="myFunction()">
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-482-5</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - HVS 70 gsm</p>
+                <p class='price'><b>Harga: </b>Rp. 150.000,00</p>
+                ">ISBN</option>
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-481-8</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - PDF</p>
+                <p class='price'><b>Harga: </b>Rp. 75.000,00</p>
+                ">E-ISBN</option>
+                        </select>
+                        <script>
+                            function myFunction() {
+                                var x = document.getElementById("id_buku").value;
+                                document.getElementById("select").innerHTML = x;
+                            }
+                            myFunction();
+                        </script>
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+        <div class="grid-content">
+            <div class="info">
+                <p class="date">Januari 2022</p>
+                <p class="author" title="Bambang Prawiro"><b>Author: </b><a href="google.com">Bambang Prawiro</a></p>
+            </div>
+            <h1 title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis"><a class="super">[100] </a><a href="www.google.com">Teknik Praktis Pengecatan Imunohistokimia Imunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisUntuk Penelitian dan Aplikasi Klinis</a></h1>
+            <hr>
+            <table class="info-content">
+                <tr>
+                    <td class="thumbnail">
+                        <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
+                    </td>
+                    <td class="specification">
+                        <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+                            <hr>
+                        <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
+                        <p class="pages"><b>Pages: </b>500</p>
+                        <div id="select"></div>
+                        <hr>
+
+                        <select id="id_buku" onchange="myFunction()">
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-482-5</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - HVS 70 gsm</p>
+                <p class='price'><b>Harga: </b>Rp. 150.000,00</p>
+                ">ISBN</option>
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-481-8</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - PDF</p>
+                <p class='price'><b>Harga: </b>Rp. 75.000,00</p>
+                ">E-ISBN</option>
+                        </select>
+                        <script>
+                            function myFunction() {
+                                var x = document.getElementById("id_buku").value;
+                                document.getElementById("select").innerHTML = x;
+                            }
+                            myFunction();
+                        </script>
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+        <div class="grid-content">
+            <div class="info">
+                <p class="date">Januari 2022</p>
+                <p class="author" title="Bambang Prawiro"><b>Author: </b><a href="google.com">Bambang Prawiro</a></p>
+            </div>
+            <h1 title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis"><a class="super">[100] </a><a href="www.google.com">Teknik Praktis Pengecatan Imunohistokimia Imunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisImunohistokimia Untuk Penelitian dan Aplikasi KlinisUntuk Penelitian dan Aplikasi Klinis</a></h1>
+            <hr>
+            <table class="info-content">
+                <tr>
+                    <td class="thumbnail">
+                        <img src="book.jpg" title="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis" alt="Teknik Praktis Pengecatan Imunohistokimia Untuk Penelitian dan Aplikasi Klinis">
+                    </td>
+                    <td class="specification">
+                        <p class="synopsis" title="Synopsis"><b>Sinopsis - </b>Pneumonia komunitas masih menjadi penyebab rawat inap dan kematian di dunia dengan tingkat insidensi yang cukup tinggi. Penyebab tingginya angka kematian pneumonia komunitas adalah akibat respons inflamasi yang cukup tinggi. Respons inflamasi yang berlebihan akan menyebabkan kerusakan paru sehingga inflamasi atau peradangan perlu dikurangi atau dikontrol. Terapi antiinflamasi dapat menjadi terapi tambahan pada pneumonia komunitas karena mampu mengurangi komplikasi respons inflamasi sistemik yang terlalu besar tanpa mengganggu proses resolusi inflamasi lokal dan menurunkan angka kematian pasien pneumonia. Pemberian terapi tambahan antiinflamasi pada berbagai macam penyakit telah dilakukan dan menunjukkan hasil yang menjanjikan. Buku ini mencakup penatalaksanaan pneumonia komunitas dengan antiinflamasi sebagai terapi tambahan, semoga buku ini bermanfaat bagi sejawat sekalian.
+                            <hr>
+                        <p class="publisher" title="UNS Press"><b>Publisher: </b><a href="google.com">UNS Press</a></p>
+                        <p class="pages"><b>Pages: </b>500</p>
+                        <div id="select"></div>
+                        <hr>
+
+                        <select id="id_buku" onchange="myFunction()">
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-482-5</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - HVS 70 gsm</p>
+                <p class='price'><b>Harga: </b>Rp. 150.000,00</p>
+                ">ISBN</option>
+                            <option value="
+                <p class='isbn'><b>ISBN: </b>978-602-397-481-8</p>
+                <p class='size'><b>Size: </b>16cm X 25cm</p>
+                <p class='gsm'>548 Pages - PDF</p>
+                <p class='price'><b>Harga: </b>Rp. 75.000,00</p>
+                ">E-ISBN</option>
+                        </select>
+                        <script>
+                            function myFunction() {
+                                var x = document.getElementById("id_buku").value;
+                                document.getElementById("select").innerHTML = x;
+                            }
+                            myFunction();
+                        </script>
+
+                    </td>
+                </tr>
+            </table>
+        </div>
     </section>
 </body>
