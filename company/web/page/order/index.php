aff6c433c5be6f5d9479191d39913598b9ac1883
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-23 21:14:09 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   company/web/page/order/index.php

--- company/web/page/order/index.php
+++ company/web/page/order/index.php
@@ -1,45 +1,34 @@
 <?php if ($member['id_part'] == "3") {
 
+    include '../src/config/database.php';
     if (isset($_POST['tambah'])) {
-        $order_books = $_POST['title_books'];
-        $title_books = $_POST['judul'];
+        $order_books = $_POST['order_books'];
+        $title_books = $_POST['title_books'];
 
         $data[] = $order_books;
         $data[] = $title_books;
 
-        $sql =
-            'INSERT INTO kelengkapan (kelengkapan_file,
-        kelengkapan_editor,
-        kelengkapan_foto,
-        kelengkapan_kata_pengantar,
-        kelengkapan_cv_bio,
-        kelengkapan_daftar_isi,
-        kelengkapan_sinopsis,
-        kelengkapan_daftar_pustaka)
-        VALUES (0,0,0,0,0,0,0,0);
-        SET @last_id_kelengkapan=LAST_INSERT_ID();
-INSERT INTO kk (id_kelengkapan,
-kk_no_order,
-kk_judul,
-VALUES(@last_id_kelengkapan,?,?);
-SET @last_id_kk=LAST_INSERT_ID()';
+        $sql9 = 'INSERT INTO completeness (file_completeness, editor_completeness, photo_completeness, foreword_completeness, cvbio_completeness, toc_completeness, synopsis_completeness, bibliografi_completeness)
+    VALUES (0, 0, 0, 0, 0, 0, 0, 0);
+    SET @last_id=LAST_INSERT_ID();
+    
+INSERT INTO books (order_books, title_books, id_completeness)
+VALUES (?, ?, @last_id)';
+        $row9 = $dbdata->prepare($sql9);
+        $row9->execute($data);
 
-        $sql_log =
-            'INSERT INTO log (id_member, id_action, log_judul, log_tanggal)
-VALUE (?, 1, ?, ?);';
+        $id_member = $_SESSION['id_member'];
+        $data2[] = $title_books;
 
-        $row = $dbdata->prepare($sql);
-        $row->execute($data);
-        $log = $dbinfo->prepare($sql_log);
-        $log->execute($data_log);
-        echo '<script>window.location="../' . $_SESSION['section'] . '/index.php?page=data"</script>';
+        $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, note_log) VALUES ($id_member, 1, 4, ?) ";
+        $query = $dbuser->prepare($log);
+        $query->execute($data2);
+        echo '<script>alert("Berhasil Tambah Data");window.location="?page=order"</script>';
     } ?>
 
-
-
     <form method="POST" action="">
-        <input type="text" name="order_books" value="" placeholder="Nomer Order"><br>
-        <input type="number" name="title_books" value="" placeholder="Judul Buku">
+        <input type="number" name="order_books" value="" placeholder="Nomer Order"><br>
+        <input type="text" name="title_books" value="" placeholder="Judul Buku">
         <input type="submit" name="tambah" value="Tambah" />
     </form>
 
