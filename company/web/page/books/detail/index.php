7839213be005ad5be3c0720ddcd48c1c8e3bd4de
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-26 09:06:54 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   company/web/page/books/detail/index.php
    	modified:   company/web/page/order/index.php
    	new file:   image/profil/2_IMG_20151023_103915.jpg
    	deleted:    image/profil/2_zerotwo.png
    	deleted:    image/profil/3_2.png
    	new file:   image/profil/3_Logo Magerlur.png

--- company/web/page/books/detail/index.php
+++ company/web/page/books/detail/index.php
@@ -4,12 +4,37 @@ $id_books = $_GET['book'];
 $books_dashboard = new view($dbdata);
 $book = $books_dashboard->books_dashboard($id_books); ?>
 
-<?php echo $book['id_books'] ?><br>
-<?php echo $book['title_books'] ?><br>
-<?php echo $book['price_books'] ?><br>
-<?php echo $book['total_books'] ?><br>
-<?php echo $book['amount_books'] ?><br>
-<?php echo $book['estimation_books'] ?><br>
+
+<form method="POST">
+    <input value="<?php echo $book['order_books'] ?>" disabled>
+    <input type="number" name="order_books" value="<?php echo $book['order_books'] ?>" placeholder="order_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
+<form method="POST">
+    <input value="<?php echo $book['title_books'] ?>" disabled>
+    <input type="text" name="title_books" value="<?php echo $book['title_books'] ?>" placeholder="title_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
+<form method="POST">
+    <input value="<?php echo $book['price_books'] ?>" disabled>
+    <input type="number" name="price_books" value="<?php echo $book['price_books'] ?>" placeholder="price_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
+<form method="POST">
+    <input value="<?php echo $book['total_books'] ?>" disabled>
+    <input type="number" name="price_books" value="<?php echo $book['total_books'] ?>" placeholder="total_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
+<form method="POST">
+    <input value="<?php echo $book['amount_books'] ?>" disabled>
+    <input type="number" name="amount_books" value="<?php echo $book['amount_books'] ?>" placeholder="amount_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
+<form method="POST">
+    <input value="<?php echo $book['estimation_books'] ?>" disabled>
+    <input type="text" name="estimation_books" value="<?php echo $book['estimation_books'] ?>" placeholder="estimation_books">
+    <input type="submit" name="#" value="Edit" />
+</form>
 
 <?php echo $book['file_completeness'] ?><br>
 <?php echo $book['editor_completeness'] ?><br>
--- company/web/page/order/index.php
+++ company/web/page/order/index.php
@@ -9,63 +9,58 @@
         $amount_books = $_POST['amount_books'];
         $estimation_books = $_POST['estimation_books'];
 
-        $data[] = $order_books;
-        $data[] = $title_books;
-        $data[] = $price_books;
-        $data[] = $total_books;
-        $data[] = $amount_books;
-        $data[] = $estimation_books;
+        $create_books[] = $order_books;
+        $create_books[] = $title_books;
+        $create_books[] = $price_books;
+        $create_books[] = $total_books;
+        $create_books[] = $amount_books;
+        $create_books[] = $estimation_books;
 
-        $sql9 = 'INSERT INTO completeness (file_completeness, editor_completeness, photo_completeness, foreword_completeness, cvbio_completeness, toc_completeness, synopsis_completeness, bibliografi_completeness)
+        $books_sql = 'INSERT INTO completeness (file_completeness, editor_completeness, photo_completeness, foreword_completeness, cvbio_completeness, toc_completeness, synopsis_completeness, bibliografi_completeness)
     VALUES (0, 0, 0, 0, 0, 0, 0, 0);
     SET @id_completeness=LAST_INSERT_ID();
 
     INSERT INTO cover (front_cover, back_cover)
     VALUES (0, 0);
     SET @id_cover=LAST_INSERT_ID();
-
+    
     INSERT INTO books (order_books, title_books, price_books, total_books, amount_books, estimation_books, id_completeness, id_cover)
     VALUES (?, ?,?, ?, ?, ?, @id_completeness, @id_cover)';
 
-        $row9 = $dbdata->prepare($sql9);
-        $row9->execute($data);
+        $row = $dbdata->prepare($books_sql);
+        $row->execute($create_books);
 
-        $id_member = $_SESSION['id_member'];
-        $data2[] = $title_books;
-
-        $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, note_log) VALUES ($id_member, 1, 4, ?) ";
-        $query = $dbuser->prepare($log);
-        $query->execute($data2);
+        $id_target = $dbdata->lastInsertId();
+        $title_log[] = $title_books;
+        $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, id_target, note_log) VALUES ($id_member, 1, 4, $id_target, ?) ";
+        $query = $dblog->prepare($log);
+        $query->execute($title_log);
         echo '<script>alert("Berhasil Tambah Data");window.location="?page=order"</script>';
     }
 
     if (isset($_POST['tambah_penulis'])) {
         $author = $_POST['author'];
 
-        $data3[] = $author;
+        $create_author[] = $author;
+        $author_sql = 'INSERT INTO member (name_member, id_part) VALUES (?, 5)';
+        $row = $dbuser->prepare($author_sql);
+        $row->execute($create_author);
 
-        $sql9 = 'INSERT INTO member (name_member, id_part)
-    VALUES (?, 5)';
-
-        $row9 = $dbuser->prepare($sql9);
-        $row9->execute($data3);
-
-        $id_member = $_SESSION['id_member'];
-        $data4[] = $author;
-
-        $log2 = "INSERT INTO com_log.log (id_member, id_crud, id_action, note_log) VALUES ($id_member, 1, 5, ?) ";
-        $query = $dbuser->prepare($log2);
-        $query->execute($data4);
+        $id_author = $dbuser->lastInsertId();
+        $author_log[] = $author;
+        $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, id_target, note_log) VALUES ($id_member, 1, 5, $id_author, ?) ";
+        $query = $dblog->prepare($log);
+        $query->execute($author_log);
         echo '<script>alert("Berhasil Tambah Penulis");window.location="?page=order"</script>';
     } ?>
 
     <form method="POST" action="">
-        <input type="number" name="order_books" value="" placeholder="Nomer Order"><br>
-        <input type="text" name="title_books" value="" placeholder="Judul Buku"><br>
-        <input type="number" name="price_books" value="" placeholder="Harga Satuan"><br>
-        <input type="number" name="total_books" value="" placeholder="Harga Semua"><br>
-        <input type="number" name="amount_books" value="" placeholder="Jumlah Cetak"><br>
-        <input type="date" name="estimation_books" value="" placeholder="Perkiraan Jadi"><br>
+        <input type="number" name="order_books" placeholder="Nomer Order" required><br>
+        <input type="text" name="title_books" placeholder="Judul Buku" required><br>
+        <input type="number" name="price_books" placeholder="Harga Satuan"><br>
+        <input type="number" name="total_books" placeholder="Harga Semua"><br>
+        <input type="number" name="amount_books" placeholder="Jumlah Cetak"><br>
+        <input type="date" name="estimation_books" placeholder="Perkiraan Jadi"><br>
         <input type="submit" name="tambah_order" value="tambah order" />
     </form>
 
