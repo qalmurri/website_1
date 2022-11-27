2c565c371810f21f3b7555e478e59c24ba65e03b
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-27 13:47:58 +0700

    modified:   src/function/view.php

--- src/function/view.php
+++ src/function/view.php
@@ -13,7 +13,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function books_web()
     {
         $sql = "SELECT * FROM com_data.books ORDER BY com_data.books.id_books DESC";
@@ -22,7 +21,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function books_dashboard($id_books)
     {
         $sql = "SELECT * FROM com_data.books INNER JOIN com_data.completeness ON com_data.books.id_completeness = com_data.completeness.id_completeness INNER JOIN com_data.cover ON com_data.books.id_cover = com_data.cover.id_cover WHERE com_data.books.id_books=?";
@@ -31,7 +29,6 @@
         $query = $row->fetch();
         return $query;
     }
-
     function section($id_section)
     {
         $sql = "SELECT * FROM section WHERE id_section=?";
@@ -40,7 +37,6 @@
         $query = $row->fetch();
         return $query;
     }
-
     function setting($id_setting)
     {
         $sql = "SELECT * FROM setting WHERE id_setting=?";
@@ -49,7 +45,6 @@
         $query = $row->fetch();
         return $query;
     }
-
     function member($id_member)
     {
         $sql = "SELECT * FROM member WHERE id_member=?";
@@ -58,7 +53,6 @@
         $query = $row->fetch();
         return $query;
     }
-
     function author()
     {
         $sql = "SELECT * FROM com_user.member WHERE com_user.member.id_part=5 ORDER BY com_user.member.name_member ASC";
@@ -67,7 +61,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function author_book($id_books)
     {
         $sql = "SELECT * FROM com_category.author INNER JOIN com_user.member ON com_category.author.id_member = com_user.member.id_member WHERE id_books=? ORDER BY com_user.member.name_member ASC";
@@ -76,7 +69,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function crud_log()
     {
         $sql = "SELECT * FROM com_log.log INNER JOIN com_user.member ON com_log.log.id_member = com_user.member.id_member ORDER BY com_log.log.id_log DESC";
@@ -85,7 +77,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function create_log()
     {
         $sql = "SELECT * FROM com_log.log INNER JOIN com_user.member ON com_log.log.id_member = com_user.member.id_member WHERE com_log.log.id_crud=1 ORDER BY com_log.log.id_log DESC";
@@ -94,7 +85,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function read_log()
     {
         $sql = "SELECT * FROM com_log.log INNER JOIN com_user.member ON com_log.log.id_member = com_user.member.id_member WHERE com_log.log.id_crud=2 ORDER BY com_log.log.id_log DESC";
@@ -103,7 +93,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function update_log()
     {
         $sql = "SELECT * FROM com_log.log INNER JOIN com_user.member ON com_log.log.id_member = com_user.member.id_member WHERE com_log.log.id_crud=3 ORDER BY com_log.log.id_log DESC";
@@ -112,7 +101,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function delete_log()
     {
         $sql = "SELECT * FROM com_log.log INNER JOIN com_user.member ON com_log.log.id_member = com_user.member.id_member WHERE com_log.log.id_crud=4 ORDER BY com_log.log.id_log DESC";
@@ -121,7 +109,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function member_log()
     {
         $sql = "SELECT * FROM com_user.member INNER JOIN com_user.part ON com_user.member.id_part = com_user.part.id_part ORDER BY com_user.member.id_member DESC";
@@ -130,7 +117,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function author_log()
     {
         $sql = "SELECT * FROM com_user.member INNER JOIN com_user.part ON com_user.member.id_part = com_user.part.id_part WHERE com_user.member.id_part=5 ORDER BY com_user.member.id_member DESC";
@@ -139,7 +125,6 @@
         $query = $row->fetchAll();
         return $query;
     }
-
     function order()
     {
         $sql = "SELECT * FROM com_data.books ORDER BY com_data.books.id_books DESC";
