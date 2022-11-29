c67ec8bec92332151887d4b39a586cb8f339f078
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-29 15:19:27 +0700

    Update index.php

--- company/web/page/order/index.php
+++ company/web/page/order/index.php
@@ -2,22 +2,31 @@
 
     if (isset($_POST['tambah_order'])) {
         $order_books = $_POST['order_books'];
-        $title_books = $_POST['title_books'];
-        $price_books = $_POST['price_books'];
-        $total_books = $_POST['total_books'];
-        $amount_books = $_POST['amount_books'];
-        $estimation_books = $_POST['estimation_books'];
-        $show_books = $_POST['show_books'];
 
-        $create_books[] = $order_books;
-        $create_books[] = $title_books;
-        $create_books[] = $price_books;
-        $create_books[] = $total_books;
-        $create_books[] = $amount_books;
-        $create_books[] = $estimation_books;
-        $create_books[] = $show_books;
+        $process = "SELECT * FROM books WHERE order_books=$order_books";
+        $stmt = $dbdata->prepare($process);
+        $stmt->execute();
 
-        $books_sql = 'INSERT INTO completeness (file_completeness, editor_completeness, photo_completeness, foreword_completeness, cvbio_completeness, toc_completeness, synopsis_completeness, bibliografi_completeness)
+        if ($stmt->rowCount() > 0) {
+            echo 'No Order Sudah Ada';
+        } else {
+            $order_books = $_POST['order_books'];
+            $title_books = $_POST['title_books'];
+            $price_books = $_POST['price_books'];
+            $total_books = $_POST['total_books'];
+            $amount_books = $_POST['amount_books'];
+            $estimation_books = $_POST['estimation_books'];
+            $show_books = $_POST['show_books'];
+
+            $create_books[] = $order_books;
+            $create_books[] = $title_books;
+            $create_books[] = $price_books;
+            $create_books[] = $total_books;
+            $create_books[] = $amount_books;
+            $create_books[] = $estimation_books;
+            $create_books[] = $show_books;
+
+            $books_sql = 'INSERT INTO completeness (file_completeness, editor_completeness, photo_completeness, foreword_completeness, cvbio_completeness, toc_completeness, synopsis_completeness, bibliografi_completeness)
     VALUES (0, 0, 0, 0, 0, 0, 0, 0);
     SET @id_completeness=LAST_INSERT_ID();
 
@@ -28,15 +37,16 @@
     INSERT INTO books (order_books, title_books, price_books, total_books, amount_books, estimation_books, id_completeness, id_cover, show_books)
     VALUES (?, ?,?, ?, ?, ?, @id_completeness, @id_cover, ?)';
 
-        $row = $dbdata->prepare($books_sql);
-        $row->execute($create_books);
+            $row = $dbdata->prepare($books_sql);
+            $row->execute($create_books);
 
-        $id_target = $dbdata->lastInsertId();
-        $title_log[] = $title_books;
-        $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, id_target, note_log) VALUES ($id_member, 1, 4, $id_target, ?) ";
-        $query = $dblog->prepare($log);
-        $query->execute($title_log);
-        echo '<script>alert("Berhasil Tambah Data");window.location="?page=order"</script>';
+            $id_target = $dbdata->lastInsertId();
+            $title_log[] = $title_books;
+            $log = "INSERT INTO com_log.log (id_member, id_crud, id_action, id_target, note_log) VALUES ($id_member, 1, 4, $id_target, ?) ";
+            $query = $dblog->prepare($log);
+            $query->execute($title_log);
+            echo '<script>alert("Berhasil Tambah Data");window.location="?page=order"</script>';
+        }
     }
 
     if (isset($_POST['tambah_penulis'])) {
