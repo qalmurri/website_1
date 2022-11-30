b82fb2253d6ff5df4c430c31a4ad18b0da6b0f9d
qalmurri <86642474+qalmurri@users.noreply.github.com>
2022-11-30 22:19:24 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/css/style.css
    	modified:   src/include/home/index.php

--- src/include/home/index.php
+++ src/include/home/index.php
@@ -1,1 +1,22 @@
-Home Web
+<div class="wrapper">
+    <section class="columns">
+
+        <div class="column">
+            <h2>1st Content Area</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto necessitatibus cum praesentium dolor totam voluptatibus recusandae?</p>
+        </div>
+
+        <div class="column">
+            <h2>2nd Content Area</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto necessitatibus cum praesentium dolor totam voluptatibus recusandae? Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum.</p>
+        </div>
+
+        <div class="column">
+            <h2>3rd Content Area</h2>
+            <p>Illo quod nemo ratione itaque dolores laudantium error vero laborum blanditiis nostrum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ratione architecto cum praesentium voluptatibus recusandae?</p>
+        </div>
+
+
+
+    </section>
+</div>
